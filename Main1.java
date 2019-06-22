29a069d8b3f98c88ed806df074df1da63eebb127
Ashwath <ashwathas1@gmail.com>
2019-06-22 18:45:29 +0530

    second commit

--- Main1.java
+++ Main1.java
@@ -2,6 +2,9 @@ class Main1
 {
 		public static void main(String args[])
 		{
-				System.out.println("Wel Come ! my first program");
+				for(int i=0;i<5;i++)
+				{
+					System.out.println("*");
+				}
 		}
 }
